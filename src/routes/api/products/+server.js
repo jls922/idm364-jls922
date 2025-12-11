382e8c90b635b3afd45d1b1f85eac7c76046d2c9
jls922 <jls922@drexel.edu>
2025-12-11 06:15:51 -0500

    a

--- src/routes/api/products/+server.js
+++ src/routes/api/products/+server.js
@@ -1,15 +1,14 @@
-import { supabase } from '$lib/supabaseClient'
-import { json } from '@sveltejs/kit'
+import { supabase } from '$lib/supabaseClient';
+import { json } from '@sveltejs/kit';
 
 export async function GET() {
-  const { data: products, error } = await supabase
-    .from('products')
-    .select('*')
-    .order('id')
-  
-  if (error) {
-    return json({ error: error.message }, { status: 500 })
-  }
-  
-  return json(products)
+    const { data, error } = await supabase
+        .from('products')
+        .select('*');
+
+    if (error) {
+        return json({ error: error.message }, { status: 500 });
+    }
+
+    return json(data);
 }
