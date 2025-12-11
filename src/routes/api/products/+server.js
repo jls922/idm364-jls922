7a3a25aa59fc224fd974fdccc979fa4ea1d94cce
jls922 <jls922@drexel.edu>
2025-12-11 17:04:49 -0500

    Update +server.js

--- src/routes/api/products/+server.js
+++ src/routes/api/products/+server.js
@@ -1,13 +1,14 @@
-import { PUBLIC_SUPABASE_TABLE_ } from "$env/static/public";
 import { json } from '@sveltejs/kit';
 import { supabase } from '$lib/supabaseClient';
 
 export async function GET() {
-  const { data, error } = await supabase.from(PUBLIC_SUPABASE_TABLE_).select('*');
+    const { data, error } = await supabase
+        .from('products')
+        .select('*');
 
-  if ( error ) {
-    return json({ error: error.message }, { status: 500 })
-  }
+    if (error) {
+        return json({ error: error.message }, { status: 500 });
+    }
 
-  return json(data, { status: 200 });
-};
+    return json(data);
+}
