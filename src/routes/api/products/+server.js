50650aa38a0bf0255b71194e7666d64c75934233
jls922 <jls922@drexel.edu>
2025-12-11 17:11:08 -0500

    frogs

--- src/routes/api/products/+server.js
+++ src/routes/api/products/+server.js
@@ -3,7 +3,7 @@ import { supabase } from '$lib/supabaseClient';
 
 export async function GET() {
     const { data, error } = await supabase
-        .from('products')
+        .from('Frogs')
         .select('*');
 
     if (error) {
