bee6f546cf5899922a0306e8d496c4821196a716
jls922 <jls922@drexel.edu>
2025-12-11 17:04:04 -0500

    a

--- src/routes/api/products/+server.js
+++ src/routes/api/products/+server.js
@@ -1,14 +1,13 @@
+import { PUBLIC_SUPABASE_TABLE_ } from "$env/static/public";
 import { json } from '@sveltejs/kit';
 import { supabase } from '$lib/supabaseClient';
 
 export async function GET() {
-    const { data, error } = await supabase
-        .from('products')
-        .select('*');
+  const { data, error } = await supabase.from(PUBLIC_SUPABASE_TABLE_).select('*');
 
-    if (error) {
-        return json({ error: error.message }, { status: 500 });
-    }
+  if ( error ) {
+    return json({ error: error.message }, { status: 500 })
+  }
 
-    return json(data);
-}
+  return json(data, { status: 200 });
+};
