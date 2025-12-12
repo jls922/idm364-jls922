458068b88997b5b15d8d2b9bae1f810b66251185
jls922 <jls922@drexel.edu>
2025-12-12 11:51:16 -0500

    a

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,15 +1,15 @@
 <script>
     import { addToBag } from '$lib/stores/cart.svelte';
-    export let id, item_name, price;
-    const product = { id, item_name, price };
+    export let id, item_name, price, image;
+    const product = { id, item_name, price, image };
 </script>
 
-<a class="card-link" href={`/shop/${id}`}>
+<a class="card-link" href="/shop/{id}">
     <div class="product-card">
-        <img src={`../../static/${image}`} alt="{item_name}" />
+        <img src="/static/{image}" alt={item_name} />
         <div class="product-info">
             <h4>{item_name}</h4>
-            <h6>{price}</h6>
+            <p>{price}</p>
         </div>
         <button on:click|preventDefault={() => addToBag(product)} class="add-to-cart">
             <img src="/images/shopping-bag.png" alt="Shopping Bag"/>
@@ -17,3 +17,23 @@
         </button>
     </div>
 </a>
+
+<!--<script>
+    import { addToBag } from '$lib/stores/cart.svelte';
+    export let id, item_name, price, image;
+    const product = { id, item_name, price, image };
+</script>
+
+<a class="card-link" href={`/shop/${id}`}>
+    <div class="product-card">
+        <img src={`../../static/${image}`} alt="{item_name}" />
+        <div class="product-info">
+            <h4>{item_name}</h4>
+            <p>{price}</p>
+        </div>
+        <button on:click|preventDefault={() => addToBag(product)} class="add-to-cart">
+            <img src="/images/shopping-bag.png" alt="Shopping Bag"/>
+            <h6>Add to Bag</h6>
+        </button>
+    </div>
+</a>-->
